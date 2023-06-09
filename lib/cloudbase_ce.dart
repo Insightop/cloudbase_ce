919b711815c259afe199ad8b9cdf1701ee46f4e4
book诗意 <tech.gosling0d@icloud.com>
2023-06-09 19:09:52 +0800

    fix spell error

--- lib/cloudbase_ce.dart
+++ lib/cloudbase_ce.dart
@@ -1,8 +1,8 @@
-/// orgin cloudbase_core pub package: https://pub.dev/packages/cloudbase_core
-/// orgin cloudbase_auth pub package: https://pub.dev/packages/cloudbase_auth
-/// orgin cloudbase_storage pub package: https://pub.dev/packages/cloudbase_storage
-/// orgin cloudbase_function pub package: https://pub.dev/packages/cloudbase_function
-/// orgin cloudbase_database pub package: https://pub.dev/packages/cloudbase_database
+/// origin cloudbase_core pub package: https://pub.dev/packages/cloudbase_core
+/// origin cloudbase_auth pub package: https://pub.dev/packages/cloudbase_auth
+/// origin cloudbase_storage pub package: https://pub.dev/packages/cloudbase_storage
+/// origin cloudbase_function pub package: https://pub.dev/packages/cloudbase_function
+/// origin cloudbase_database pub package: https://pub.dev/packages/cloudbase_database
 /// author: https://cloudbase.net/ & lirongcong.bennett@gmail.com
 
 library cloudbase_ce;
